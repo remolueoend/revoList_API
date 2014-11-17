04bc7186bcb4eb9ab4f61f003025bd0f32ca562e
Remo Lüönd <remo.zumsteg@outlook.com>
2014-11-17 22:41:16 +0100

    added submodule revoList and searchController

--- controllers/BaseController.js
+++ controllers/BaseController.js
@@ -4,9 +4,18 @@
 
 'use strict';
 
+var inherit = require('node-inherit').inherit;
+
 function BaseController(context){
     this.context = context;
     this.db = context.db;
 }
+
+BaseController.inherit = function(prototype){
+
+    return inherit(BaseController, function(context){
+        BaseController.call(this, context);
+    }, prototype);
+};
 
 module.exports = BaseController;
---
+++ controllers/SearchController.js
@@ -0,0 +1,13 @@
+/**
+ * Created by remo on 17/11/14.
+ */
+
+'use strict';
+
+module.exports = require('./BaseController').inherit({
+
+    get: function(query){
+
+    }
+
+});
